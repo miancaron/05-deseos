3fc6762633b8c9d65ae32a05620bc0c989338a69
miguel_casas <miguel.casas@atmira.com>
2020-06-09 18:25:48 +0200

    Uso del Local Storage (guardar y recuperar) y terminada pantalla de nueva lista

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,14 +11,57 @@ import { Router } from '@angular/router';
 export class Tab1Page {
 
   constructor( public deseosService: DeseosService ,
-               private router: Router) {
-
+               private router: Router,
+               private alertCtrl: AlertController) {
 
   }
 
-  agregarLista () {
+  // async convierte el método a una promesa
+  async agregarLista() {
 
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+    // this.router.navigateByUrl('/tabs/tab1/agregar');
+
+      const alert = await this.alertCtrl.create({
+        header: 'Nueva lista',
+        inputs: [
+          {
+            name: 'titulo',
+            type: 'text',
+            placeholder: 'Nombre de la lista'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancelar',
+            // role es el tipo de botón: 'cancel' indica que es botón de cancelar y eso implica,
+            // por ejemplo, que cuando se pincha fuera de la alerta también se lanza el handler
+            // de este botón.
+            role: 'cancel',
+            // handler: lo que hace cuando se pulsa el botón
+            handler: () => {
+              console.log('Cancelar');
+            }
+          },
+          {
+            text: 'Crear',
+            handler: ( data ) => {
+              // data son los datos del formulario
+              console.log( data );
+              // si no se ha escrito nada
+              if ( data.titulo.length === 0 ){
+                //no hacemos nada
+                return;
+              }
+
+              // Crear la lista
+              this.deseosService.crearLista( data.titulo );
+
+            }
+          }
+        ]
+      });
+  
+      alert.present();
 
   }
 
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -10,10 +10,40 @@ export class DeseosService {
 
   constructor() { 
 
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Héroes a desaparecer');
+    this.cargarStorage();
 
-    this.listas.push(lista1, lista2);
+    // const lista1 = new Lista('Recolectar piedras del infinito');
+    // const lista2 = new Lista('Héroes a desaparecer');
+
+    // this.listas.push(lista1, lista2);
+
+  }
+
+  crearLista( titulo: string ) {
+    const nuevaLista = new Lista( titulo );
+    this.listas.push( nuevaLista );
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+
+    // JSON.stringify -> convierte un objeto a un JSON (string)
+    localStorage.setItem( 'data', JSON.stringify(this.listas) );
+
+  }
+
+  cargarStorage() {
+
+    if ( localStorage.getItem( 'data' ) ) {
+
+      // JSON.parse -> convierte un string JSON a objeto
+      this.listas = JSON.parse( localStorage.getItem( 'data' ) );
+
+    } else {
+
+      this.listas = [];
+
+    }
 
   }
 }
